8948799b7c5535914d740fd24c75bb352f5979f5
Kangning Li <likangning93@gmail.com>
2015-09-13 18:33:10 -0400

    isolated upsweep and downsweep. I think it should be faster this way, less memcpy

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -49,9 +49,22 @@ void scan(int n, int *odata, const int *idata) {
 
 	cudaMemcpy(dev_x, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
 
-    // Up Sweep
+	// up sweep and down sweep
+	up_sweep_down_sweep(pow2, dev_x);
+
+	cudaMemcpy(odata, dev_x, sizeof(int) * n, cudaMemcpyDeviceToHost);
+	cudaFree(dev_x);
+}
+
+// exposed up sweep and down sweep. expects powers of two!
+void up_sweep_down_sweep(int n, int *dev_data) {
+	int logn = ilog2ceil(n);
+	dim3 dimBlock(n);
+	dim3 dimGrid(1);
+
+	// Up Sweep
 	for (int d = 0; d < logn; d++) {
-		upsweep_step <<<dimGrid, dimBlock>>>(d, dev_x);
+		upsweep_step <<<dimGrid, dimBlock >>>(d, dev_data);
 	}
 
 	//debug: peek at the array after upsweep
@@ -61,13 +74,10 @@ void scan(int n, int *odata, const int *idata) {
 	// Down-Sweep
 	int zero[1];
 	zero[0] = 0;
-	cudaMemcpy(&dev_x[pow2 - 1], zero, sizeof(int) * 1, cudaMemcpyHostToDevice);
+	cudaMemcpy(&dev_data[n - 1], zero, sizeof(int) * 1, cudaMemcpyHostToDevice);
 	for (int d = logn - 1; d >= 0; d--) {
-		downsweep_step <<<dimGrid, dimBlock>>>(d, dev_x);
+		downsweep_step << <dimGrid, dimBlock >> >(d, dev_data);
 	}
-
-	cudaMemcpy(odata, dev_x, sizeof(int) * n, cudaMemcpyDeviceToHost);
-	cudaFree(dev_x);
 }
 
 __global__ void temporary_array(int *x, int *temp) {
@@ -97,42 +107,42 @@ __global__ void scatter(int *x, int *trueFalse, int* scan, int *out) {
  * @returns      The number of elements remaining after compaction.
  */
 int compact(int n, int *odata, const int *idata) {
-	dim3 dimBlock(n);
+	int logn = ilog2ceil(n);
+	int pow2 = (int)pow(2, logn);
+
+	dim3 dimBlock(pow2);
 	dim3 dimGrid(1);
 	int *dev_x;
 	int *dev_tmp;
-	cudaMalloc((void**)&dev_x, sizeof(int) * n);
-	cudaMalloc((void**)&dev_tmp, sizeof(int) * n);
+	int *dev_scatter;
+	int *dev_scan;
 
+	cudaMalloc((void**)&dev_x, sizeof(int) * pow2);
+	cudaMalloc((void**)&dev_tmp, sizeof(int) * pow2);
+	cudaMalloc((void**)&dev_scan, sizeof(int) * pow2);
+	cudaMalloc((void**)&dev_scatter, sizeof(int) * pow2);
+
+	// 0 pad up to a power of 2 array length.
 	// copy everything in idata over to the GPU.
-	cudaMemcpy(dev_x, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+	fill_by_value << <dimGrid, dimBlock >> >(0, dev_x);
+	cudaMemcpy(dev_x, idata, sizeof(int) * pow2, cudaMemcpyHostToDevice);
 
     // Step 1: compute temporary true/false array
-	temporary_array << <dimGrid, dimBlock >> >(dev_x, dev_tmp);
+	temporary_array <<<dimGrid, dimBlock >>>(dev_x, dev_tmp);
 
 	// Step 2: run efficient scan on the tmp array
-	// TODO: expose the CUDA relevant portions of efficient scan so we don't have to shunt around
-	int *trueArray = new int[n];
-	int *scanArray = new int[n];
-	cudaMemcpy(trueArray, dev_tmp, sizeof(int) * n, cudaMemcpyDeviceToHost);
-	scan(n, scanArray, trueArray);
+	cudaMemcpy(dev_scan, dev_tmp, sizeof(int) * pow2, cudaMemcpyDeviceToDevice);
+		up_sweep_down_sweep(pow2, dev_scan);
 
 	// Step 3: scatter
-	int *dev_scatter;
-	cudaMalloc((void**)&dev_scatter, sizeof(int) * n);
+	scatter <<<dimGrid, dimBlock >>>(dev_x, dev_tmp, dev_scan, dev_scatter);
 
-	int *dev_scan;
-	cudaMalloc((void**)&dev_scan, sizeof(int) * n);
-	cudaMemcpy(dev_scan, scanArray, sizeof(int) * n, cudaMemcpyHostToDevice);
+	cudaMemcpy(odata, dev_scatter, sizeof(int) * pow2, cudaMemcpyDeviceToHost);
 
-	scatter << <dimGrid, dimBlock >> >(dev_x, dev_tmp, dev_scan, dev_scatter);
+	int return_value;
+	cudaMemcpy(&return_value, dev_scan + (n - 1), sizeof(int),
+		cudaMemcpyDeviceToHost);
 
-	cudaMemcpy(odata, dev_scatter, sizeof(int) * n, cudaMemcpyDeviceToHost);
-
-	int return_value = scanArray[n - 1];
-
-	delete trueArray;
-	delete scanArray;
 	cudaFree(dev_x);
 	cudaFree(dev_tmp);
 	cudaFree(dev_scan);
--- stream_compaction/efficient.h
+++ stream_compaction/efficient.h
@@ -4,6 +4,8 @@ namespace StreamCompaction {
 namespace Efficient {
     void scan(int n, int *odata, const int *idata);
 
+	void up_sweep_down_sweep(int n, int *dev_data);
+
     int compact(int n, int *odata, const int *idata);
 }
 }
