6671b7a716fde2e1d10b1e743ba33c8a294a870b
Kangning Li <likangning93@gmail.com>
2015-09-13 16:29:47 -0400

    added more tests, efficient scan largely done. TODO: address power of 2 problems

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -8,12 +8,56 @@ namespace Efficient {
 
 // TODO: __global__
 
+__global__ void upsweep_step(int d, int *x) {
+	int k = threadIdx.x + (blockIdx.x * blockDim.x);
+	if (k % (int) powf(2, d + 1)) {
+		return;
+	}
+	x[k + (int) powf(2, d + 1) - 1] += x[k + (int) powf(2, d) - 1];
+}
+
+__global__ void downsweep_step(int d, int *x) {
+	int k = threadIdx.x + (blockIdx.x * blockDim.x);
+	if (k % (int)powf(2, d + 1)) {
+		return;
+	}
+	int t = x[k + (int) powf(2, d) - 1];
+	x[k + (int) powf(2, d) - 1] = x[k + (int) powf(2, d + 1) - 1];
+	x[k + (int) powf(2, d + 1) - 1] += t;
+}
+
 /**
  * Performs prefix-sum (aka scan) on idata, storing the result into odata.
  */
 void scan(int n, int *odata, const int *idata) {
-    // TODO
-    printf("TODO\n");
+	// copy everything in idata over to the GPU
+	dim3 dimBlock(n);
+	dim3 dimGrid(1);
+	int *dev_x;
+	cudaMalloc((void**)&dev_x, sizeof(int) * n);
+
+	cudaMemcpy(dev_x, idata, sizeof(int) * n, cudaMemcpyHostToDevice);
+
+    // Up Sweep
+	int logn = ilog2ceil(n);
+	for (int d = 0; d < logn; d++) {
+		upsweep_step <<<dimGrid, dimBlock>>>(d, dev_x);
+	}
+
+	//debug: peek at the array after upsweep
+	//int peek[8];
+	//cudaMemcpy(&peek, dev_x, sizeof(int) * 8, cudaMemcpyDeviceToHost);
+
+	// Down-Sweep
+	int zero[1];
+	zero[0] = 0;
+	cudaMemcpy(&dev_x[n - 1], zero, sizeof(int) * 1, cudaMemcpyHostToDevice);
+	for (int d = logn - 1; d >= 0; d--) {
+		downsweep_step <<<dimGrid, dimBlock>>>(d, dev_x);
+	}
+
+	cudaMemcpy(odata, dev_x, sizeof(int) * n, cudaMemcpyDeviceToHost);
+	cudaFree(dev_x);
 }
 
 /**
